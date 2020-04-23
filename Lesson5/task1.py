e76fc80b578f9fa38a6a101b74314aa67cd75568
lapikit <ggopher@yandex.ru>
2020-04-23 16:56:56 +0300

    task6 undone

--- Lesson5/task1.py
+++ Lesson5/task1.py
@@ -1,4 +1,4 @@
-with open('task1.txt', 'w', encoding='UTF-8') as file:
+with open('task1.txt', 'w', encoding='UTF-8') as file:      #w, чтобы файл заново перезаписывался с нуля
     while True:
         text = input()
         if text!='':
