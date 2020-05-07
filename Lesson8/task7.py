1e326b636c51e01f538eecc816ec840f16a6fdb0
lapikit <ggopher@yandex.ru>
2020-05-07 18:09:09 +0300

    homework lesson 8, almost done

--- Lesson8/task7.py
+++ Lesson8/task7.py
@@ -4,3 +4,22 @@
 Проверьте работу проекта, создав экземпляры класса (комплексные числа) и выполнив сложение и умножение созданных экземпляров.
 Проверьте корректность полученного результата.
 """
+
+class Complex():
+    def __init__(self, digit):
+        self.digit = digit
+
+    def __add__(self, other):
+        return  self.digit + other.digit
+
+    def __mul__(self, other):
+        return  self.digit * other.digit
+
+
+complex1 = Complex(1 +2j)
+complex2 = Complex(2 +3j)
+
+complex3 = complex1 + complex2
+complex4 = complex1 * complex2
+print(complex3)
+print(complex4)
